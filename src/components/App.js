58c984059cb844604ccb714c2f7bea76a9504510
Reshav Thakur <Reshavthkr@gmail.com>
2021-05-13 13:21:20 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,26 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.relativelist = [
+            {name: 'Relative1'},
+            {name: 'Relative2'},
+            {name: 'Relative3'} 
+        ]
+    }
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+                <ol>
+                    {
+                    this.relativelist.map((data, i) => {
+                        
+                        return <li key={i}>{data.name}</li>;
+                    })
+                    }
+                </ol>
             </div>
         )
     }
